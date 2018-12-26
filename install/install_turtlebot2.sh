bd912ee80a8ba0fa94b386eea56394d43f2255ef
gaunthan <gaunthanhuang@foxmail.com>
2018-12-26 11:14:25 +0800

    Update repo if exist.

--- install/install_turtlebot2.sh
+++ install/install_turtlebot2.sh
@@ -5,7 +5,11 @@ sudo apt-get install ros-melodic-joy -y
 
 # Install turtlebot2 packages from source
 cd ../src
-git clone https://github.com/turtlebot/turtlebot.git
+if [ ! -d turtlebot ]; then
+    git clone https://github.com/turtlebot/turtlebot.git
+else
+    git pull
+fi
 cd ..
 catkin_make
 source ./devel/setup.bash
