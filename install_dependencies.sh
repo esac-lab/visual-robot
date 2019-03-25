f44c653051b2dbaefa57ba7337145459eb7d2921
gaunthan <gaunthanhuang@foxmail.com>
2019-03-25 22:13:40 +0800

    Move denpendencies' installation to individual file

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env sh
 
+cd ./install
+
 ./install_ros.sh
-
 ./install_ORB_SLAM2.sh
-
 ./install_cuda_10.sh
-
 ./install_zed.sh
-
 ./install_kinect.sh
-
 ./install_turtlebot2.sh
--- make_robot.sh
+++ make_robot.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env sh
 
-mkdir -p src
-
-# Install all deps.
-cd ./install
-./install_all.sh
-cd -
-
 # Build ORB_SLAM
 cd src/ORB_SLAM2
 ./build.sh
